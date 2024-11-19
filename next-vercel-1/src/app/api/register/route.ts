664ca9626a455a3373b278baf6994ec2d4a39faf
Daruma.K <k.net.game01@gmail.com>
2024-11-19 21:01:31 +0700

    initial vommit

---
+++ next-vercel-1/src/app/api/register/route.ts
@@ -0,0 +1,29 @@
+import User from "@/models/User";
+import connect from "@/utils/db";
+import bcrypt from "bcryptjs";
+import { NextResponse } from "next/server";
+
+export const POST = async (request: any) => {
+    const { email, password } = await request.json();
+
+    await connect();
+
+    const existingUser = await User.findOne({ email });
+
+    if (existingUser) {
+        return new NextResponse("User already exists", { status: 400 });
+    }
+
+    const hashedPassword = await bcrypt.hash(password, 5);
+    const newUser = new User({
+        email,
+        password: hashedPassword,
+    })
+    
+    try {
+        await newUser.save();
+        return new NextResponse("User created successfully", { status: 200 });
+    } catch (err: any) {
+        return new NextResponse(err.message, { status: 500 });
+    }
+};
